b5e6f73afcc9f40f200955b254c1971bc2c4d411
Marmajan Rokaya <marmajanrokaya6@gmail.com>
2026-08-14 15:50:15 +0545

    changes

--- server/mysewa_backend/settings.py
+++ server/mysewa_backend/settings.py
@@ -172,21 +172,18 @@ WSGI_APPLICATION = 'mysewa_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+   
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysewa',
+        'USER': 'root',
+        'PASSWORD': '06517349bd1633ec',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'mysewa',
-    #     'USER': 'root',
-    #     'PASSWORD': '06517349bd1633ec',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306',
-    #     'OPTIONS': {
-    #         'charset': 'utf8mb4',
-    #     },
-    # }
 }
 
 # Shared file cache so OTP / rate-limit keys work across multiple workers.
